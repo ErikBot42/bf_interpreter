abf6bc4a0ad43a4e3b3bcf393d8b228fca1025d7
Erik Magnusson <erik.arn.magnusson@gmail.com>
2023-07-08 11:52:53 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,9 @@
 use std::io::Read;
 use std::io::Write;
+use std::time::Instant;
 
-#[derive(Debug, Eq, PartialEq)]
-enum Opcode {
-    Add(u8),
-    /// Open
-    BranchZero(u32),
-    /// Close
-    BranchNotZero(u32),
-    Right(i32),
-    Dot,
-    Comma,
 
-    Clear,
-    AddTo(i32),
-    Seek(i32),
-}
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 enum BasicOpcode {
     Add,
     Sub,
@@ -28,7 +15,7 @@ enum BasicOpcode {
     Comma,
 }
 
-const DATA_LEN: usize = 1 << 20;
+const DATA_LEN: usize = 1 << 16;
 
 fn to_basic_opcode(c: u8) -> Option<BasicOpcode> {
     match c {
@@ -44,15 +31,52 @@ fn to_basic_opcode(c: u8) -> Option<BasicOpcode> {
     }
 }
 
+trait BfEngine {
+    type OPCODE;
+    fn generate(iter: impl Iterator<Item = BasicOpcode>) -> Vec<Self::OPCODE>;
+    fn execute(opcodes: &[Opcode], data: &mut [u8; DATA_LEN], input: &[u8], stdout: &mut std::io::StdoutLock<'static>);
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+enum Opcode {
+    Add(u8),
+    /// Open
+    BranchZero(u16),
+    /// Close
+    BranchNotZero(u16),
+    Right(i16),
+    Dot,
+    Comma,
+
+    Clear,
+    AddTo(i16),
+    Seek(i16),
+}
+
+enum NewOpcode {
+    //Add(u8),
+    //Right(i16),
+    AddRight(u8, i16),
+
+    //Clear,
+    //AddTo(i16),
+    //Set(u8),
+    SetAddTo(u8, i16), 
+
+    Seek(i16),
+    
+    BranchZero(u16),
+    BranchNotZero(u16),
+    Dot,
+    Comma,
+}
+
 fn generate_opcodes(
     mut iter: impl Iterator<Item = BasicOpcode>,
 ) -> Result<Vec<Opcode>, &'static str> {
-    let mut top_level_loops: Vec<String> = Vec::new();
-
     let mut buffer = Vec::new();
     use Opcode::*;
-
-    let mut toplevel = true;
+    let mut toplevel = false;
     let mut open_stack: Vec<usize> = Vec::new();
     loop {
         let Some(opcode) = iter.next() else { break };
@@ -87,14 +111,14 @@ fn generate_opcodes(
                 }
             }
             BasicOpcode::Open => {
-                toplevel = true;
                 open_stack.push(buffer.len());
                 buffer.push(BranchZero(0));
+                toplevel = true;
             }
             BasicOpcode::Close => {
                 let other = open_stack.pop().ok_or("unbalanced brackets: extra ]")?;
                 let this = buffer.len();
-                buffer[other] = BranchZero(this as u32);
+                buffer[other] = BranchZero(this.try_into().unwrap());
 
                 match &buffer[..] {
                     &[.., BranchZero(_), Add(255)] => {
@@ -109,10 +133,9 @@ fn generate_opcodes(
                         buffer.push(Seek(x));
                     }
                     _ => {
-                        //buffer.push(BranchNotZero(-diff));
-                        buffer.push(BranchNotZero(other as u32));
+                        buffer.push(BranchNotZero(other.try_into().unwrap()));
                         if toplevel {
-                            top_level_loops.push(opcodes_to_string(&buffer[other..=this]));
+                            println!("{}", opcodes_to_string(&buffer[other..]));
                         }
                     }
                 }
@@ -121,12 +144,15 @@ fn generate_opcodes(
             BasicOpcode::Dot => buffer.push(Dot),
             BasicOpcode::Comma => buffer.push(Comma),
         }
-        if let Some(Add(0) | Right(0)) = buffer.last() {
-            let _ = buffer.pop();
+        match &buffer[..] {
+            &[.., Add(0)] | &[.., Right(0)] | &[.., AddTo(_), Clear] => {
+                println!("found redundant code {:?}", buffer);
+                let _ = buffer.pop();
+            }
+            _ => (),
         }
     }
-    top_level_loops.sort();
-    dbg!(top_level_loops);
+
     if !open_stack.is_empty() {
         return Err("unbalanced brackets: extra [");
     } else {
@@ -143,32 +169,52 @@ fn main() -> Result<(), &'static str> {
         .filter_map(Result::ok)
         .filter_map(to_basic_opcode);
 
-    let input_args = args.next();
-    let input = input_args
-        .as_ref()
-        .map(String::as_str)
-        .unwrap_or("")
-        .as_bytes();
+    let input = args.next().unwrap_or(String::new());
 
-    let program_basic_opcodes: Vec<_> = basic_opcodes_iter.collect();
+    let opcodes: Vec<_> = basic_opcodes_iter.collect();
 
-    //let data_len = 1_usize << 24;
+    let basic_opcodes_iter = opcodes.iter().copied();
 
-    let opcodes = generate_opcodes(program_basic_opcodes.iter().copied())?;
+    let start = Instant::now();
+    println!("generating opcodes...");
+    let opcodes = generate_opcodes(basic_opcodes_iter)?;
+    println!("generating done in {:?}", start.elapsed());
 
-    //println!("{:?}", &opcodes);
+    {
+        let mut stats = std::collections::HashMap::new();
+        let opcodes: Vec<_> = opcodes
+            .iter()
+            .map(|opcode| match opcode {
+                Opcode::Add(_) => '+',
+                Opcode::BranchZero(_) => '[',
+                Opcode::BranchNotZero(_) => ']',
+                Opcode::Right(_) => '>',
+                Opcode::Dot => '.',
+                Opcode::Comma => ',',
+                Opcode::Clear => 'C',
+                Opcode::AddTo(_) => 'M',
+                Opcode::Seek(_) => 'S',
+            })
+            .collect();
+        for w in opcodes.windows(2) {
+            *stats.entry(w).or_insert(0) += 1
+        }
+        let mut stats: Vec<_> = stats.iter().map(|(a, b)| (*a, *b)).collect();
+        stats.sort_by_key(|s| s.1);
+        dbg!(stats);
+    }
 
+    let start = Instant::now();
     println!("allocating buffer...");
     let mut data: Box<[u8; DATA_LEN]> = alloc_const();
-    println!("allocation done...");
+    println!("allocation done in {:?}", start.elapsed());
 
     println!("{}", opcodes_to_string(&opcodes));
+    println!("{} instructions generated", opcodes.len());
 
-    let start = std::time::Instant::now();
-    //execute(&opcodes, &mut data, input);
-    
+    let start = Instant::now();
     let stdout = &mut std::io::stdout().lock();
-    execute(&opcodes, &mut data, input, stdout);
+    execute(&opcodes, &mut data, input.as_bytes(), stdout);
     println!("elapsed: {:?}", start.elapsed());
 
     Ok(())
@@ -221,7 +267,12 @@ fn opcodes_to_string(opcodes: &[Opcode]) -> String {
 }
 
 #[inline(never)]
-fn execute(opcodes: &[Opcode], data: &mut [u8; DATA_LEN], input: &[u8], stdout: &mut std::io::StdoutLock<'static>) {
+fn execute(
+    opcodes: &[Opcode],
+    data: &mut [u8; DATA_LEN],
+    input: &[u8],
+    stdout: &mut std::io::StdoutLock<'static>,
+) {
     let mut input = input.iter().copied();
     let mut pc: usize = 0;
     let mut dp: usize = 0;
@@ -254,13 +305,12 @@ fn execute(opcodes: &[Opcode], data: &mut [u8; DATA_LEN], input: &[u8], stdout: 
             Opcode::Clear => data[dp as usize] = 0,
             Opcode::AddTo(i) => {
                 let from = dp as usize;
-                let to = (dp.wrapping_add(*i as _)) as usize;
+                let to = ((dp.wrapping_add(*i as _)) as usize) % DATA_LEN;
 
                 let tmp = data[from];
-                if tmp != 0 {
-                    data[from] = data[from].wrapping_sub(tmp);
-                    data[to] = data[to].wrapping_add(tmp);
-                }
+
+                data[from] = tmp.wrapping_sub(tmp);
+                data[to] = data[to].wrapping_add(tmp);
             }
             Opcode::Seek(i) => {
                 while data[dp as usize] != 0 {
